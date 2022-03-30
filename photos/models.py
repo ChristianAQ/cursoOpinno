229ed0ef0bb2f53d7f3a2c8b8cec6c4b40b07e3e
ChristianAQ <christian.desd@outlook.es>
2022-03-30 10:30:24 +0200

    creacion de models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Photo(models.Model):
+
+    LICENSES = (
+        ('RIG', 'Copyright'),
+        ('LEF', 'Copyleft'),
+        ('CC', 'Creative commons')
+    )
+
+    name = models.CharField(max_length=150)
+    url = models.URLField()
+    description = models.TextField()
+    license = models.CharField(max_length=3, choices=LICENSES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    #las dps ultimas lineas son poner la fecha creada y actualizada automaticamente
+
+    def __str__(self):
+        return self.name
