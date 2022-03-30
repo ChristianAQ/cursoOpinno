f3659c8eef2a9c6e43ca545adfa9a974f09070b7
ChristianAQ <christian.desd@outlook.es>
2022-03-30 12:40:07 +0200

    creacion de templates

--- photos/views.py
+++ photos/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from photos.models import Photo
 
 
 # Create your views here.
-def hello(request):
+def saludo(request):
     nombre = request.GET.get('nombre')
     apellido = request.GET.get('apellido')
     return HttpResponse("Hello {0} {1}".format(nombre, apellido))
 
+
+def home(request):
+    """
+    Renderiza el home con un lisado de fotos
+    :param request: objeto HtttpRequest con los datos de la peticion
+    :return: objeto HttpResponse con los datos de la respuesta
+    """
+    photos = Photo.objects.all() #Recupera todas las foos de la base de datos
+    context = {'photos_list': photos}
+    return render(request, 'photos/home.html', context)
